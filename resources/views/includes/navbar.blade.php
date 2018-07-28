240ba8616b4ecb95d2730eb1fc6fbc1b5fec001d
saif-git <saifeddine.jouini@tek-up.tn>
2018-07-28 10:39:29 +0200

    my new update on 28/07/2018

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,97 +1,103 @@
+ <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>-->
+      
+      <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">-->
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <form class="form-inline" action="/action_page.php">
-    <ul class="navbar-nav">
+    <!-- For Materila design  CSS   and JAVA SCRiPT -->
 
-        <li class="nav-item active">
-            <a class="nav-link" href="#">My_website</a>
-          </li>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="img/icon1.png" width="30" height="30" alt="">
+  </a>  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+     <li class="nav-item">
+        <a class="nav-link disabled" href="#">My Site</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">My acount <span class="sr-only">(current)</span></a>
+      </li>
       <li class="nav-item">
         <a class="nav-link" href="/">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/about">About</a>
       </li>
-   
-    <li class="nav-item">
-      <a class="nav-link" href="/prolanguage">Prog_Language</a>
-    </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/prolanguage">Prog_Language</a>
+      </li>
 
-
-  </ul>
-  <div class="decalage2">
-    <input class="form-control mr-sm-2 " type="text" placeholder="Search">
-    <button class="btn btn-success " type="submit">Search</button>
-  </div>
-
-    <div class="dropdown decalage">
-        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Options
-        </button>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">login</a>
-          <a class="dropdown-item" href="#">registery</a>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Registery</a>
+          <a class="dropdown-item" href="#">Login</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Log out</a>
         </div>
-      </div>
-
-  </form>
-
-  <style>
-
-    .decalage{
-      left: 90%;
-    }
-
-    .decalage2{
-      left:60%;
-    }
-
-    .decalage3{
-      left:70%;
-    }
-    .middle{
-    
-      left: 30%;
-    }
-  </style>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">My acount</a>
+      </li>
+    </ul>
+    <form class="form-inline my-3 my-lg-5">
+      <input class="form-control mr-sm-3 " type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
 
 </nav>
+
+
 <div id="demo" class="carousel slide" data-ride="carousel">
 
-    <!-- Indicators -->
-    <ul class="carousel-indicators">
-      <li data-target="#demo" data-slide-to="0" class="active"></li>
-      <li data-target="#demo" data-slide-to="1"></li>
-      <li data-target="#demo" data-slide-to="2"></li>
-      <li data-target="#demo" data-slide-to="3"></li>
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
 
-    </ul>
+
   
-    <!-- The slideshow -->
-    <div class="carousel-inner middle">
-      <div class="carousel-item active">
-        <img src="img/ace.jpg" alt="Los Angeles">
-      </div>
-      <div class="carousel-item">
-        <img src="img/marco.jpg" alt="Chicago">
-      </div>
-      <div class="carousel-item">
-        <img src="img/mihawk.jpg" alt="New York">
-      </div>
-      <div class="carousel-item">
-        <img src="img/zoro1.jpg" alt="California">
+  <div class="carousel-inner center1">
+    <div class="carousel-item active">
+      <img src="img/ace.jpg" alt="Los Angeles">
+      <div class="carousel-caption ">
+        <h3>First one</h3>
+        <p>We had such a great time in LA!</p>
       </div>
     </div>
-  
-    <!-- Left and right controls -->
-    <a class="carousel-control-prev" href="#demo" data-slide="prev">
-      <span class="carousel-control-prev-icon"></span>
-    </a>
-    <a class="carousel-control-next" href="#demo" data-slide="next">
-      <span class="carousel-control-next-icon"></span>
-    </a>
-  
+    <div class="carousel-item center1">
+      <img src="img/10.jpg" alt="Chicago">
+    </div>
+    <div class="carousel-item center1">
+      <img src="img/12.jpg" alt="New York">
+    </div>
   </div>
 
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<style>
+  .center1{
+    position: center;
+    left: 20%;
+  }
+</style>
 
